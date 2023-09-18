f1e36b64ff2b0cdb00c7eb3e579a9b442d680022
Abdulrahman <rahhal.abdelrahman@gmail.com>
2023-09-18 14:16:31 +0300

    Add receiveAttack to Gameboard.js and test it

--- src/__tests__/gameboard.test.js
+++ src/__tests__/gameboard.test.js
@@ -1,14 +1,14 @@
-import { Gameboard } from '../factories/Gameboard';
+import { Gameboard } from '../factories/Gameboard'
 
-describe.only('Gameboard module tests', () => {
+describe('placeShip function tests', () => {
   let gameboard;
   const length = 4;
   const [x, y] = [0, 0];
   const direction = 'h';
 
   beforeEach(() => {
-  gameboard = Gameboard();
-  gameboard.placeShip(x, y, length, direction);
+    gameboard = Gameboard();
+    gameboard.placeShip(x, y, length, direction);
   })
 
   test("should have ship's tiles not empty", () => {
@@ -31,14 +31,14 @@ describe.only('Gameboard module tests', () => {
   test('should have correct ship tile indexes if horizontal', () => {
     Array.from({ length: length }).forEach((_, i) => {
       if (direction === 'v') return;
-      expect(gameboard.board[x][y + i][1]).toBe(i);
+      expect(gameboard.board[x][y + i][2]).toBe(i);
     });
   });
 
   test('should have correct ship tile indexes if vertical', () => {
     Array.from({ length: 4 }).forEach((_, i) => {
       if (direction === 'h') return;
-      expect(gameboard.board[x + i][y][1]).toBe(i);
+      expect(gameboard.board[x + i][y][2]).toBe(i);
     });
   });
 
@@ -54,3 +54,27 @@ describe.only('Gameboard module tests', () => {
     });
   });
 });
+
+describe.only('receiveAttack', () => {
+  let g;
+
+  beforeEach(() => {
+    g = Gameboard();
+    g.placeShip(0, 0, 4, 'v');
+  })
+
+  test("should hit the correct tile and change its status", () => {
+    const x = 0;
+    const y = 0;
+    expect(g.board[x][y][1]).toBeFalsy();
+    g.receiveAttack(0, 0);
+    expect(g.board[x][y][1]).toBeTruthy();
+  })
+
+  test("should not change tile if already hit", () => {
+    const x = 0;
+    const y = 0;
+    g.receiveAttack(0, 0);
+    expect(g.board[x][y][1]).toBeTruthy();
+  })
+})
--- src/factories/Gameboard.js
+++ src/factories/Gameboard.js
@@ -3,14 +3,15 @@ import { Ship } from './Ship'
 const Gameboard = () => {
 
   const boardSize = 10;
-  const board = Array.from({ length: boardSize }, _  =>
-    Array.from({ length: boardSize }, _ => ['empty', 0])
+  const board = Array.from({ length: boardSize }, _ =>
+    // [['empty' or ship, isHit: true | false, null if empty or index of ship]]
+    Array.from({ length: boardSize }, _ => ['empty', false, null])
   )
 
 
   const isReserved = (x, y, length, direction) => {
     for (let i = 0; i < length; i++) {
-      if(direction === 'v' && board[x + i][y][0] !== 'empty'
+      if (direction === 'v' && board[x + i][y][0] !== 'empty'
         || direction === 'h' && board[x][y + i][0] !== 'empty') {
         return true;
       }
@@ -31,10 +32,10 @@ const Gameboard = () => {
         y >= boardSize) return;
 
       for (let i = 0; i < length; i++) {
-        board[x + i][y] = [ship, i]
+        board[x + i][y] = [ship,false, i]
       }
     }
-    else if (direction === 'h'){
+    else if (direction === 'h') {
       if (
         x < 0 ||
         x >= boardSize ||
@@ -42,14 +43,27 @@ const Gameboard = () => {
         y > boardSize - length) return;
 
       for (let i = 0; i < length; i++) {
-        board[x][y + i] = [ship, i]
+        board[x][y + i] = [ship,false, i]
       }
     }
+  }
+
+  const receiveAttack = (x, y) => {
+    const target = board[x][y];
+
+    if (target[1] === true) return;
+
+    if (target[0] === 'empty') {
+      target[1] = true;
+    }
+    target[0].hit(target[2])
+    target[1] = true;
   }
 
   return {
     placeShip,
     board,
+    receiveAttack,
   }
 }
 
