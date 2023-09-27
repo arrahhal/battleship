cdffa3fcaa3ef08cc7493f5499f5bdde93b96ade
Abdulrahman <rahhal.abdelrahman@gmail.com>
2023-09-27 19:48:09 +0300

    Add Game.js to handle players and boards

---
+++ src/Game.js
@@ -0,0 +1,24 @@
+import { Gameboard } from './factories/Gameboard'
+import { Player } from './factories/Player'
+import { TurnManager } from './helpers/TurnManager'
+
+export const Game = (() => {
+  let player;
+  let opponent;
+  let playerGameboard;
+  let opponentGameboard;
+
+  const init = () => {
+    player = Player('you');
+    opponent = Player('opponent');
+    playerGameboard = Gameboard();
+    opponentGameboard = Gameboard();
+    TurnManager.init(player, opponent, playerGameboard, opponentGameboard);
+  }
+
+  return {
+    init,
+    playerBoard: () => playerGameboard.board,
+    opponentBoard: () => opponentGameboard.board
+  }
+})();
