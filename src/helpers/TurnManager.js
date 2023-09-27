d8fee025951a162e44cf2c51908c06649d90b7d1
Abdulrahman <rahhal.abdelrahman@gmail.com>
2023-09-27 17:06:42 +0300

    Add TurnManager.js to handle player turns

---
+++ src/helpers/TurnManager.js
@@ -0,0 +1,39 @@
+export const TurnManager = (() => {
+  let currentPlayer;
+  let nextPlayer;
+  let currentGameboard;
+  let nextGameboard;
+  let isGameOver = false;
+
+  const init = (player1, player2, gameboard1, gameboard2) => {
+    currentPlayer = player1;
+    nextPlayer = player2;
+    currentGameboard = gameboard1;
+    nextGameboard = gameboard2;
+    isGameOver = false;
+  };
+
+  const getCurrentPlayer = () => currentPlayer;
+  const getNextPlayer = () => nextPlayer;
+  const isGameover = () => isGameOver;
+
+  const endTurn = () => {
+    if (currentGameboard.isAllShipsSunk()) {
+      isGameOver = true;
+    }
+    const temp = currentPlayer;
+    currentPlayer = nextPlayer;
+    nextPlayer = temp;
+    const tempGameboard = currentGameboard;
+    currentGameboard = nextGameboard;
+    nextGameboard = tempGameboard;
+  };
+
+  return {
+    init,
+    getCurrentPlayer,
+    getNextPlayer,
+    isGameover,
+    endTurn,
+  };
+})();
