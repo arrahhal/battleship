3287351a8d3ebcb7c8cddad1ca97f2f38573c1d2
Abdulrahman <rahhal.abdelrahman@gmail.com>
2023-09-27 12:08:36 +0300

    Add helpers/selector.js

---
+++ src/helpers/selectors.js
@@ -0,0 +1,9 @@
+export const selectors = {
+  tablePlayer: document.getElementById('table-player'),
+  tableOpponent: document.getElementById('table-opponent'),
+  btnRandomize: document.getElementById('btn-randomize'),
+  btnReset: document.getElementById('btn-reset'),
+  btnStart: document.getElementById('btn-start'),
+  log: document.getElementById('log')
+}
+
