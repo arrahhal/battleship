28cfcfc34c19fb71705d1bcd47037d7b3373ff4d
Abdulrahman <rahhal.abdelrahman@gmail.com>
2023-09-28 08:59:39 +0300

    Refactor 'matrixToTable'
    - show ships on player board
    - fix: call ship.isSunk to add appropriet class

--- src/DOM.js
+++ src/DOM.js
@@ -1,13 +1,13 @@
 import { selectors } from './helpers/selectors'
 
 const DOM = (() => {
-  const generateTableFromMatrix = (matrix = []) => {
+  const matrixToTable = (matrix = [], isShipsShown = false) => {
     let tableBody = document.createElement('tbody');
     const table = document.createElement('table');
     table.appendChild(tableBody);
     matrix.forEach((row, rowIndex) => {
       let tableRow = document.createElement('tr');
-      row.forEach((col, colIndex) => tableRow.innerHTML += `<td class="${col.isHit ? (col.occupant ? (col.isSunk ? 'sunk' : 'hit') : 'mis') : ''}" data-x="${colIndex}" data-y="${rowIndex}"></td>`)
+      row.forEach((col, colIndex) => tableRow.innerHTML += `<td class="${col.isHit ? (col.occupant ? (col.occupant.isSunk() ? 'sunk' : 'hit') : 'mis') : ''}${isShipsShown ? (col.occupant ? 'ship' : '') : ''}" data-x="${colIndex}" data-y="${rowIndex}"></td>`)
       tableBody.appendChild(tableRow);
     })
     return table;
